afa5f2a0dd130b30d5a4074a2dc29b1964e89f35
João Abreu <jgabreu2007@gmail.com>
2026-09-22 00:16:28 -0300

    feat: complete SubjectService with approveand reject method, add status validation exceptions

---
+++ src/main/java/com/joaoguilherme/tutoringmanagementsystem/exception/SubjectNotFoundException.java
@@ -0,0 +1,7 @@
+package com.joaoguilherme.tutoringmanagementsystem.exception;
+
+public class SubjectNotFoundException extends RuntimeException {
+  public SubjectNotFoundException(String message) {
+    super(message);
+  }
+}
