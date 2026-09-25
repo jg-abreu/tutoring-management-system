c98b2ac6b16de28272b1ee4fcae52431baa5a7a3
João Abreu <jgabreu2007@gmail.com>
2026-09-24 21:17:55 -0300

    test: add success test for approveTutoringBond

--- src/test/java/com/joaoguilherme/tutoringmanagementsystem/service/TutoringBondServiceTest.java
+++ src/test/java/com/joaoguilherme/tutoringmanagementsystem/service/TutoringBondServiceTest.java
@@ -1,12 +1,24 @@
 package com.joaoguilherme.tutoringmanagementsystem.service;
 
+import com.joaoguilherme.tutoringmanagementsystem.model.Subject;
+import com.joaoguilherme.tutoringmanagementsystem.model.TutoringBond;
+import com.joaoguilherme.tutoringmanagementsystem.model.User;
+import com.joaoguilherme.tutoringmanagementsystem.model.enums.BondStatus;
 import com.joaoguilherme.tutoringmanagementsystem.repository.SubjectRepository;
 import com.joaoguilherme.tutoringmanagementsystem.repository.TutoringBondRepository;
 import com.joaoguilherme.tutoringmanagementsystem.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class TutoringBondServiceTest {
@@ -22,4 +34,34 @@ public class TutoringBondServiceTest {
 
     @InjectMocks
     private TutoringBondService tutoringBondService;
+
+
+    @Test
+    void deveAprovarTutoringBondQuandoStatusForPending() {
+
+        UUID tutoringBondUuid = UUID.randomUUID();
+
+
+        User user = new User();
+        user.setId(UUID.randomUUID());
+
+        Subject subject = new Subject();
+        subject.setId(UUID.randomUUID());
+
+        TutoringBond tutoringBond = new TutoringBond(user, subject);
+        tutoringBond.setId(UUID.randomUUID());
+
+        User admin = new User();
+        admin.setId(UUID.randomUUID());
+
+        when(tutoringBondRepository.findById(tutoringBondUuid)).thenReturn(Optional.of(tutoringBond));
+        when(userRepository.findById(admin.getId())).thenReturn(Optional.of(admin));
+        when(tutoringBondRepository.save(any(TutoringBond.class))).thenReturn(tutoringBond);
+
+        TutoringBond tutoringBondTest = tutoringBondService.approveTutoringBond(tutoringBondUuid, admin.getId());
+
+        Assertions.assertEquals(BondStatus.APPROVED, tutoringBondTest.getStatus());
+
+        Assertions.assertEquals(admin, tutoringBondTest.getEvaluator());
+    }
 }
